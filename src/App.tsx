ecac71d69dc5bfe02af9076cd30bb50217e6528a
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-12-05 12:19:32 +0000

    add app tsx elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import QuestionCard from "./components/QuestionCard";
 
 function App() {
-  return <div className="App">Quiz</div>;
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+  return (
+    <div className="App">
+      <h1> REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions ...</p>
+    </div>
+  );
 }
 
 export default App;
