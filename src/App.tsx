45c8d85134a07cd03a933188c564c74bbf7b536f
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-12-05 13:03:19 +0000

    api.ts has been started

--- src/App.tsx
+++ src/App.tsx
@@ -25,14 +25,14 @@ function App() {
       </button>
       <p className="score">Score:</p>
       <p>Loading Questions ...</p>
-      <QuestionCard
+      {/* <QuestionCard
         questionNr={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      />
+      /> */}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
