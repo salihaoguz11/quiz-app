d62341344b3e357782a37c25ddb1121b322d5e82
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-12-06 12:03:13 +0000

    wrapper div style has been created

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -6,10 +6,40 @@ html{
     height:100%,
 }
 body{
-    background-image:url()(${BGImage});
+    background-image:url(${BGImage});
     background-size:cover;
     margin:0;
     padding:0 20px;
     display:flex;
     justify-content:center;
+}
+*{
+    box-sizing:border-box;
+    font-family:'Catamaran',sans-serif
 }`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > p {
+    color: #fff;
+  }
+  .score {
+    color: #fff;
+    font-size: 2rem;
+    margin: 0;
+  }
+  h1 {
+    font-family: Fascinate Inline;
+    background-image: linear-gradient(180deg, #fff, #87f1ff);
+    font-weight: 400;
+    background-size: 100%;
+    background-clip: text;
+    --webkit-background-clip: text;
+    --webkit-text-fill-color: transparent;
+    filter: drop-shadow() (2px 2px #0085a3);
+    margin: 20px;
+  }
+`;
