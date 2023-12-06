a875a1772cedbecef918c1e718d805fe7e9df53e
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-12-06 11:44:14 +0000

    globalStyle has been added

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,2 +1,15 @@
 import styled, { createGlobalStyle } from "styled-components";
 import BGImage from "./images/nattu-adnan-unsplash.jpg";
+
+export const GlobalStyle = createGlobalStyle`
+html{
+    height:100%,
+}
+body{
+    background-image:url()(${BGImage});
+    background-size:cover;
+    margin:0;
+    padding:0 20px;
+    display:flex;
+    justify-content:center;
+}`;
