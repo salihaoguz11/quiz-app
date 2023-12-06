f0cf5f90186fbdbf094fd94e7e37638ea871fbd6
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-12-06 12:11:48 +0000

    buttons styled

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -37,9 +37,24 @@ export const Wrapper = styled.div`
     font-weight: 400;
     background-size: 100%;
     background-clip: text;
-    --webkit-background-clip: text;
+    /* --webkit-background-clip: text;
     --webkit-text-fill-color: transparent;
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent; */
     filter: drop-shadow() (2px 2px #0085a3);
+    text-align: center;
     margin: 20px;
+  }
+
+  .start,
+  .next {
+    cursor: pointer;
+    background: linear-gradient(180deg, #fff, #ffcc91);
+    border: 2px solid #d38558;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    height: 40px;
+    margin: 20px 0;
+    padding: 0 40px;
   }
 `;
