952da32ebabcdf7a9eeb8eeb2612a9a7269104fa
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-12-06 10:40:49 +0000

    debugging

--- src/App.tsx
+++ src/App.tsx
@@ -50,19 +50,25 @@ function App() {
         </button>
       ) : null}
       {!gameOver ? <p className="score">Score:</p> : null}
-      {loading ? <p>Loading Questions ...</p> : null}
-
-      {/* <QuestionCard
-        questionNr={number + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      /> */}
-      <button className="next" onClick={nextQuestion}>
-        Next Question
-      </button>
+      {loading && <p>Loading Questions ...</p>}
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+      {!gameOver &&
+      !loading &&
+      userAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
     </div>
   );
 }
