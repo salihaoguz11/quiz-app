5919cb2de5b5727b3be36f0e1436e77ef9413110
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-12-06 09:32:44 +0000

    AnswerObject type has been created

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import React, { useState } from "react";
 import { fetchQuizQuestions } from "./API";
 import QuestionCard from "./components/QuestionCard";
 import { QuestionState, Difficulty } from "./API";
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
 const TOTAL_QUESTIONS = 10;
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
