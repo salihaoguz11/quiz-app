bf5db2aeb34d91e180c40b136bfa4b8fcaa51051
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-12-07 13:46:46 +0000

    Button wrapper has been created

--- src/components/QuestionCard.styles.ts
+++ src/components/QuestionCard.styles.ts
@@ -15,4 +15,17 @@ export const Wrapper = styled.div`
 
 export const ButtonWrapper = styled.div`
   transition: all 0.3s ease-in-out;
+  :hover {
+    opacity: 0.8;
+  }
+
+  button {
+    cursor: pointer;
+    user-select: none;
+    font-size: 0.8rem;
+    width: 100%;
+    height: 40px;
+    margin: 5px 0;
+    border: 3px solid #fff;
+  }
 `;
